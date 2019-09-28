9108b97dda4b7a77feb63c35fd7ecb39c764632e
Matthew C <m23.cullen@gmail.com>
2019-09-28 18:08:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,7 @@ class Scraper
   
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    binding.pry
   end 
   
   def print_courses
@@ -25,3 +26,4 @@ end
 
 
 
+Scraper.new.get_page
